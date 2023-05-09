e3e031d6387e288bcdcf5b021bb274921c155050
Jeevan-12 <jeevangowdabjeeva@gmail.com>
2023-05-09 16:48:03 +0530

    git commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import connection from './database/db.js';
 import Routes from './routes/route.js';
-
+const PORT = process.env.config || 8000;
 let app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', Routes);
 connection();
-app.listen(8000, console.log('data base is running'));
+app.listen(PORT, console.log('data base is running'));
 
 // import express from 'express';
 // let app = express();
